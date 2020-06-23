e549494fcb57d1bec914705acd43fced99d2b5d0
Joris Verhelst <joris.verhelst@v8897@student.hogent.be>
2020-06-23 16:54:21 +0200

    worked on views

---
+++ MediaWeb/Controllers/MediaController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using MediaWeb.Database;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MediaWeb.Controllers
+{
+    public class MediaController : Controller
+    {
+        private readonly MediaDbContext _mediaDbContext;
+
+        public MediaController(MediaDbContext context)
+        {
+            _mediaDbContext = context;
+        }
+
+
+        public IActionResult Index()
+        {
+
+            return View();
+        }
+    }
+}
