2dd77a39a1deae1ba18670d6e23859f42dd8edb6
Joris Verhelst <joris.verhelst@v8897@student.hogent.be>
2020-06-24 13:01:49 +0200

    film detail

---
+++ MediaWeb/Controllers/FilmController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using MediaWeb.Database;
+using MediaWeb.Domain;
+using MediaWeb.Models.Film;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace MediaWeb.Controllers
+{
+    public class FilmController : Controller
+    {
+        private readonly MediaDbContext _mediaDbContext;
+
+        public FilmController(MediaDbContext context)
+        {
+            _mediaDbContext = context;
+        }
+
+        public async Task<IActionResult> Detail(int id)
+        {
+            Film filmFromDb = await _mediaDbContext.Films.FindAsync(id);
+            FilmDetailViewModel model = new FilmDetailViewModel
+            {
+                Id = filmFromDb.Id,
+                Producer = filmFromDb.Producer,
+                ReleaseDate = filmFromDb.ReleaseDate,
+                Title = filmFromDb.Title,
+                Url = filmFromDb.Url
+            };
+
+            return View(model);
+        }
+    }
+}
--- MediaWeb/Models/Film/FilmDetailViewModel.cs
+++ MediaWeb/Models/Film/FilmDetailViewModel.cs
@@ -1,16 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace MediaWeb.Domain
+namespace MediaWeb.Models.Film
 {
-    public class Film
+    public class FilmDetailViewModel
     {
         public string Title { get; set; }
-
-        [Key]
         public int Id { get; set; }
         public string Producer { get; set; }
         public string Url { get; set; }
