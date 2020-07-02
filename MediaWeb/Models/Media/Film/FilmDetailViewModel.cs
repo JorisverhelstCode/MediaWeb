62fe787188990443a4ce1cea2cf0502d0ba8ffef
Joris Verhelst <joris.verhelst@v8897@student.hogent.be>
2020-07-02 09:52:22 +0200

    updated index

--- MediaWeb/Models/Media/Film/FilmDetailViewModel.cs
+++ MediaWeb/Models/Media/Film/FilmDetailViewModel.cs
@@ -1,17 +1,14 @@
-﻿using System;
+﻿using MediaWeb.Models.Media;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace MediaWeb.Models.Film
 {
-    public class FilmDetailViewModel
+    public class FilmDetailViewModel : MediaDetailViewModel
     {
-        public string Title { get; set; }
-        public int Id { get; set; }
         public string Producer { get; set; }
-        public string Url { get; set; }
-        public DateTime ReleaseDate { get; set; }
         public string Description { get; set; }
     }
 }
--- MediaWeb/Models/Media/MediaDetailViewModel.cs
+++ MediaWeb/Models/Media/MediaDetailViewModel.cs
@@ -3,12 +3,13 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace MediaWeb.Models
+namespace MediaWeb.Models.Media
 {
-    public class MediaIndexViewModel
+    public class MediaDetailViewModel
     {
         public int Id { get; set; }
         public string Title { get; set; }
-        public int MyProperty { get; set; }
+        public string Url { get; set; }
+        public DateTime ReleaseDate { get; set; }
     }
 }
